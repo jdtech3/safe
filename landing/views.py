42a646264fc842b90937231b29e6154bf5777c31
jdtech3 <dm.joedai@gmail.com>
2023-03-25 04:05:16 +0000

    Add actual landing page

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+# [/]: home
 def index(request):
-    return HttpResponse('Welcome to SAFE. <a href="charts">Charts</a> <a href="quiz">Quiz</a>')
+    return render(request, 'landing/index.html')
