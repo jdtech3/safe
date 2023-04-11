3936a43e720b88d352477c331f7d9be3bc01bda2
jdtech3 <dm.joedai@gmail.com>
2023-04-11 04:21:52 +0000

    Evaluate: add log out

--- evaluate/forms.py
+++ evaluate/forms.py
@@ -1,13 +1,6 @@
-from django.forms import *
+from django.forms import Form, CharField
 
 
 class SafetyQuizForm(Form):
     first_name = CharField(label='First name', max_length=100)
     last_name = CharField(label='Last name', max_length=100)
-    
-    # # styles
-    # def __init__(self, *args, **kwargs):
-    #     super(SafetyQuizForm, self).__init__(*args, **kwargs)
-
-    #     for f in self.visible_fields():
-    #         f.field.widget.attrs['class'] = 'form-control'
