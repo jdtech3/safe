c1bd5f756a484f6fae77ade1ad0d61911494e908
jdtech3 <dm.joedai@gmail.com>
2023-04-14 21:50:01 +0000

    Evaluate: finished form

--- evaluate/forms.py
+++ evaluate/forms.py
@@ -1,22 +1,46 @@
-from django.forms import Form, CharField, ChoiceField
+from django.forms import Form, CharField, ChoiceField, Textarea
 from django.templatetags.static import static
 
-from .widgets import PictureMCQ
+from .widgets import PictureMCQ, VideoMCQ
 
 class SafetyQuizForm(Form):
     first_name = CharField(label='First name', max_length=100, disabled=True)
     last_name = CharField(label='Last name', max_length=100, disabled=True)
 
     MCQ_CHOICES = [
+        ('-', '-'),
         ('A', 'A'),
         ('B', 'B'),
         ('C', 'C'),
         ('D', 'D'),
     ]
 
-    q_fire_hazard = ChoiceField(label='Which pictogram indicates that an object may be a fire hazard?', 
+    q_fire_hazard = ChoiceField(label='1. Which pictogram indicates that an object may be a fire hazard?', 
                                 choices=MCQ_CHOICES, 
                                 widget=PictureMCQ(picture_url=static('evaluate/assets/q_fire_hazard.png')))
-    q_env_hazard = ChoiceField(label='The image below represents what type of safety hazard?', 
+    
+    q_env_hazard = ChoiceField(label='2. The image below represents what type of safety hazard?', 
                                choices=MCQ_CHOICES, 
                                widget=PictureMCQ(picture_url=static('evaluate/assets/q_env_hazard.png')))
+    
+    FIRE_EXTINGUISHER_CHOICES = [
+        ('-', '-'),
+        ('A', '0-2 ft'),
+        ('B', '3-5 ft'),
+        ('C', '6-8 ft'),
+        ('D', '9-11 ft'),
+    ]
+    q_fire_extinguisher = ChoiceField(label='3. Watch the video. How far should you be from the fire when using a fire extinguisher?',
+                                      choices=FIRE_EXTINGUISHER_CHOICES,
+                                      widget=VideoMCQ(youtube_id='lUojO1HvC8c'))
+    
+    PASS_ACRONYM_CHOICES = [
+        ('-', '-'),
+        ('A', 'Pray And Succumb Silently'),
+        ('B', 'Pull Aim Squeeze Sweep'),
+        ('C', 'Please Always Stand Still'),
+        ('D', 'Post All Snapchat Stories'),
+    ]
+    q_pass_acronym = ChoiceField(label='4. From the video, what does the acronym P.A.S.S stand for?', choices=PASS_ACRONYM_CHOICES)
+
+    q_list_ppe = CharField(label='5. List three pieces of personal protective equipment (PPE) in the lab.', widget=Textarea(attrs={'rows': 3, 'cols': 20}))
--- evaluate/widgets.py
+++ evaluate/widgets.py
@@ -13,3 +13,17 @@ class PictureMCQ(Select):
         context['widget']['picture_url'] = self.picture_url
 
         return context
+
+class VideoMCQ(Select):
+    template_name = 'widgets/video_mcq.html'
+
+    def __init__(self, attrs=None, choices=(), youtube_id=None):
+        super().__init__(attrs, choices)
+        self.youtube_id = youtube_id
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        
+        context['widget']['youtube_id'] = self.youtube_id
+
+        return context
